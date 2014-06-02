9fefef6628df67095376584ce2eb17abd5476be1
Tang Qiao <tangqiaoboy@gmail.com>
2014-06-02 09:14:23 +0800

    MOD: add gitcafe remote.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
 
-rake generate
 rake deploy
+cd _deploy
+
+# add gitcafe source
+git remote add gitcafe git@gitcafe.com:tangqiaoboy/tangqiaoboy.git >> /dev/null 2>&1
+echo "### Pushing to GitCafe..."
+git push gitcafe master:gitcafe-pages
+echo "### Done"
